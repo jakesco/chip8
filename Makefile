310e264c5fadb39f2f423a61d127886472137252
Jake Scott <jscott@roguecu.org>
2022-06-24 14:53:38 -0700

    refactor screen example

--- Makefile
+++ Makefile
@@ -1,6 +1,10 @@
-index:
+wasm:
 	mkdir -p build
 	emcc src/*.c -o build/index.html -s USE_SDL=2
+
+native:
+	mkdir -p build
+	clang src/*.c -o build/chip8 -lSDL2
 
 serve:
 	python -m http.server --directory build/
--- src/main.c
+++ src/main.c
@@ -1,45 +1,94 @@
 #include <SDL2/SDL.h>
 #include <stdlib.h>
+#include <stdbool.h>
+#include <stdint.h>
 
 #ifdef __EMSCRIPTEN__
 #include <emscripten.h>
 #endif
 
+#include "dbg.h"
+
+#define WINDOW_WIDTH 640
+#define WINDOW_HEIGHT 320
+#define FPS 60
+
 SDL_Window *window;
 SDL_Renderer *renderer;
-SDL_Surface *surface;
+SDL_Texture *texture;
+uint32_t *color_buffer;
 
-void drawRandomPixels() {
-    if (SDL_MUSTLOCK(surface)) SDL_LockSurface(surface);
+bool setup(void) {
+    check(SDL_Init(SDL_INIT_VIDEO) == 0, "Faild to initialize SDL.\n");
 
-    Uint8 * pixels = surface->pixels;
-    
-    for (int i=0; i < 1048576; i++) {
-        char randomByte = rand() % 255;
-        pixels[i] = randomByte;
+    window = SDL_CreateWindow(
+        NULL,                   // window title
+        SDL_WINDOWPOS_CENTERED, // x pos
+        SDL_WINDOWPOS_CENTERED, // y pos
+        WINDOW_WIDTH,           // width
+        WINDOW_HEIGHT,          // height
+        SDL_WINDOW_BORDERLESS   // type
+    );
+    check(window, "Failed to create SDL window.\n");
+
+    // SDL Renderer
+    renderer = SDL_CreateRenderer(window, -1, 0);
+    check(renderer, "Failed to create SDL renderer.\n");
+
+    color_buffer = (uint32_t*) malloc(sizeof(uint32_t) * WINDOW_WIDTH * WINDOW_HEIGHT);
+    check_mem(color_buffer);
+
+    texture = SDL_CreateTexture(
+            renderer,
+            SDL_PIXELFORMAT_ARGB8888,
+            SDL_TEXTUREACCESS_STREAMING,
+            WINDOW_WIDTH,
+            WINDOW_HEIGHT
+    );
+    check(texture, "Failed to create SDL texture.\n");
+
+    return true;
+
+error:
+    return false;
+}
+
+void update(void) {
+    for (int i = 0; i < WINDOW_WIDTH * WINDOW_HEIGHT; i++) {
+        color_buffer[i] = 0xFFFFFFFF;
     }
+}
 
-    if (SDL_MUSTLOCK(surface)) SDL_UnlockSurface(surface);
+void render(void) {
+    SDL_UpdateTexture(
+            texture,
+            NULL,
+            color_buffer,
+            (int)(WINDOW_WIDTH * sizeof(uint32_t))
+    );
+    SDL_RenderCopy(renderer, texture, NULL, NULL);
 
-    SDL_Texture *screenTexture = SDL_CreateTextureFromSurface(renderer, surface);
-
-    SDL_RenderClear(renderer);
-    SDL_RenderCopy(renderer, screenTexture, NULL, NULL);
     SDL_RenderPresent(renderer);
+}
 
-    SDL_DestroyTexture(screenTexture);
+void game_loop(void) {
+    /* process_input(); */
+    update();
+    render();
 }
 
 int main(int argc, char *argv[]) {
-    SDL_Init(SDL_INIT_VIDEO);
-    SDL_CreateWindowAndRenderer(512, 512, 0, &window, &renderer);
-    surface = SDL_CreateRGBSurface(0, 512, 512, 32, 0, 0, 0, 0);
-    
+    bool initialized = setup();
+    if(!initialized) {
+        printf("Init failed.\n");
+        exit(1);
+    }
+
     #ifdef __EMSCRIPTEN__
-        emscripten_set_main_loop(drawRandomPixels, 0, 1);
+        emscripten_set_main_loop(render, 60, 1);
     #else
     while(1) {
-        drawRandomPixels();
+        render();
         SDL_Delay(16);
     }
     #endif
